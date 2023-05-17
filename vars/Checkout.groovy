bb64cd1f6247ec393925f6404f75da828f5ac587
prani <hall@ho.com>
2023-05-18 00:26:02 +0530

    add jenkins shared library files

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -1,9 +1,19 @@
-def call(repositoryUrl, branch = 'master') {
-  node {
+// Jenkinsfile.groovy
+
+def call() {
     stage('Checkout') {
-      git([url: repositoryUrl, branch: branch])
+        steps {
+            script {
+                // Define the Git credentials
+
+                // Clone the Git repository
+                git(
+                    branch: 'main',
+                    url: 'https://github.com/praaws/gite2e.git'
+                )
+            }
+        }
     }
-  }
 }
 
 return this
